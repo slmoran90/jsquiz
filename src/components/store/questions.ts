ed504c3c3e8c09f940e82be7ecdabbedffb13819
Sergio Morán <moransergio.01@gmail.com>
2025-04-20 00:12:40 -0300

    fix line endings

--- src/components/store/questions.ts
+++ src/components/store/questions.ts
@@ -13,7 +13,7 @@ interface State {
   resetGame: () => void
 }
 
-const API_URL = import.meta.env ?? 'http://localhost:5173'
+const API_URL = import.meta.env.VITE_URL ?? 'http://localhost:5173'
 
 export const useQuestionsStore = create<State>()(
   persist(
